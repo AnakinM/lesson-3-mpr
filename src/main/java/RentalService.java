12cf77fc26e5bf746803832671d230ced6cd3360
Mateusz Anikiej <mateusz@anikiej.com>
2023-11-04 15:07:31 +0100

    Added comments

--- src/main/java/RentalService.java
+++ src/main/java/RentalService.java
@@ -6,4 +6,26 @@ public class RentalService {
     // estimatePrice(vin, int days)
     // Higher prices in weekends and holidays
 
+    /*
+    rentCar(clientId, vin, startDate, endDate) {
+    czy auto istnieje
+        czy auto dostepne
+            true
+                dodajemu obiekt do rental storage
+                zwracamy rental
+            flase
+                throw error
+    }
+
+    isAvailable(vin, startDate, endDate) {
+
+    }
+
+    estimatePrice(vin, startDate, endDate) {
+    pobrac samochod
+    pobrac type samochodu z obiektu
+    obliczamy ilosc dni miedzy datami
+    ilosc dni * stala * mnoznik z type samochodu
+    }
+     */
 }
